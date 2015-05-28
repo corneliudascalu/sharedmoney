aefc3419809523e53c4c1f58665b8fb28fd4f7e9
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2015-05-29 00:01:45 +0300

    Appcompat theming

--- app/src/main/java/com/corneliudascalu/sharedmoney/MainActivity.kt
+++ app/src/main/java/com/corneliudascalu/sharedmoney/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.corneliudascalu.sharedmoney;
 
 import android.os.Bundle
-import android.support.v7.app.ActionBarActivity
-import org.jetbrains.anko.button
-import org.jetbrains.anko.onClick
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.verticalLayout
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.*
 
-class MainActivity : ActionBarActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         verticalLayout {
+            var toolbar = toolbarSupport() {
+                title = "Hello"
+                backgroundColor = R.color.material_blue_grey_800
+            }
+            setSupportActionBar(toolbar)
             button("OK?") {
                 onClick { toast("Poof!") }
             }
