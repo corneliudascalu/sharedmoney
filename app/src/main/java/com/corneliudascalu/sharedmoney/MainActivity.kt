a7103a395b39ac8c9c656b2cbca5b2e50af17a46
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2015-08-30 17:01:29 +0300

    Set up kotlin extensions correctly

--- app/src/main/java/com/corneliudascalu/sharedmoney/MainActivity.kt
+++ app/src/main/java/com/corneliudascalu/sharedmoney/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import org.jetbrains.anko.*
+import kotlinx.android.synthetic.activity_main.*;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+        
     }
 }
