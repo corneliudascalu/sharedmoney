0fdb3ad99f9bf8ae5f5ae4d188304a209bd7f311
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2015-08-30 16:22:03 +0300

    More experiments with appcompat

--- app/src/main/java/com/corneliudascalu/sharedmoney/MainActivity.kt
+++ app/src/main/java/com/corneliudascalu/sharedmoney/MainActivity.kt
@@ -2,20 +2,14 @@ package com.corneliudascalu.sharedmoney;
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        verticalLayout {
-            var toolbar = toolbarSupport() {
-                title = "Hello"
-                backgroundColor = R.color.material_blue_grey_800
-            }
-            setSupportActionBar(toolbar)
-            button("OK?") {
-                onClick { toast("Poof!") }
-            }
-        }
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
     }
 }
