88a937b4e0deb446138effced8762b57d606e985
Corneliu Dascalu <corneliu.dascalu@gmail.com>
2015-08-30 17:03:09 +0300

    Add button with toast using kotlin magic

--- app/src/main/java/com/corneliudascalu/sharedmoney/MainActivity.kt
+++ app/src/main/java/com/corneliudascalu/sharedmoney/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        
+        testButton.setOnClickListener { toast("Hey!") }
     }
 }
