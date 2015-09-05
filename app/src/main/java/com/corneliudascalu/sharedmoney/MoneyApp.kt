4aae4a8914209a672510d1173d71316c4bd464a1
Corneliu Dascalu <corneliu.dascalu@fortech.ro>
2015-09-05 22:57:35 +0300

    Use app id and client key

--- app/src/main/java/com/corneliudascalu/sharedmoney/MoneyApp.kt
+++ app/src/main/java/com/corneliudascalu/sharedmoney/MoneyApp.kt
@@ -6,9 +6,15 @@ import com.parse.Parse
 /**
  */
 public class MoneyApp : Application() {
+
+    companion object {
+        public val APPLICATION_ID: String = "5b6uY59Cwj6auWlpZbkytDrD62fHsZDWC6Ch4IuN"
+        public val CLIENT_KEY: String = "1ZaqEnGfYzGJuOES6Yci25mVPjgp78loGs2Xy7b2"
+    }
+
     override fun onCreate() {
         super.onCreate()
         Parse.enableLocalDatastore(this)
-        Parse.initialize(this)
+        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
     }
 }
